19e0c8b4406d73746a971c66f58f44aa88cde93e
skeeerr <142380754+skeeerr@users.noreply.github.com>
2025-05-31 11:32:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -127,5 +127,26 @@ async def handle_mirror(message: types.Message):
 async def handle_commands(message: types.Message):
     await message.answer(COMMANDS_TEXT)
 
+from aiogram.utils.executor import start_webhook
+
 if __name__ == "__main__":
-    executor.start_polling(dp)
+   
+    WEBHOOK_HOST = 'https://runet-failures-bot.onrender.com/'  
+    WEBHOOK_PATH = '/webhook'
+    WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+    
+
+    WEBAPP_HOST = '0.0.0.0'  
+    WEBAPP_PORT = 10000  
+    
+    async def on_startup(dp):
+        await bot.set_webhook(WEBHOOK_URL)
+        logging.info("Бот успешно запущен через вебхук")
+    
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
