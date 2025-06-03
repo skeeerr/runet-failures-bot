511c292c218124579ed64c635e52fec5a9489f74
intmdl <142380754+skeeerr@users.noreply.github.com>
2025-06-03 10:53:42 +0400

    Delete utils.py

--- utils.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Bot
-from aiogram.utils.deep_linking import get_start_link
-
-async def generate_referral_link(bot: Bot, user_id: int) -> str:
-    return await get_start_link(payload=str(user_id), encode=True)
